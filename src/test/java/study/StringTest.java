8d207d6702e56b2f3140afac789831d807350413
jeongbright <104780993+Jeong-Bright@users.noreply.github.com>
2024-11-07 21:54:36 +0900

    UnitTest 요구사항 Step1: split 학습 테스트 구현 완료

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,7 @@
 package study;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +11,25 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    @DisplayName("문자열을 쉼표를 기준으로 분리한다.")
+    void split() {
+        // given
+        String preSeparationString = "1,2";
+        // when
+        String[] separationString = preSeparationString.split(",");
+        // then
+        Assertions.assertThat(separationString).contains("1","2");
+
+        // ---
+
+        // given
+        String preSeparationStringV2 = "1";
+        // when
+        String[] separationStringV2 = preSeparationStringV2.split(",");
+        // then
+        Assertions.assertThat(separationStringV2).containsExactly("1");
     }
 }
